d6979f684716d1d6093e96a7739b11c9541af21f
rvforest <rvforest@gmail.com>
2020-12-17 21:55:44 -0500

    Day 2. Part 1

---
+++ 2/2.jl
@@ -0,0 +1,56 @@
+using Pipe
+
+LIMITS = 1
+LETTER = 2
+PW = 3
+
+
+function main()
+    println("Day 2")
+    entries = parse_input("2/input.txt")
+    println("Part 1")
+    println("Answer: $(part1(entries))")
+end
+
+
+function parse_input(input_path)
+    inputs = split.(readlines(input_path), " ")
+    parse_entry.(inputs)
+end
+
+
+function parse_entry(entry)
+    lower, upper = @pipe entry[LIMITS] |> split(_, "-") |> parse.(Int, _)
+    letter = entry[LETTER][1]
+    Entry(
+        Policy(letter, lower, upper),
+        entry[PW]
+    )
+end
+
+
+function part1(inputs)
+    sum(entry_is_valid.(inputs))
+end
+
+
+struct Policy
+    letter::Char
+    lower::Int
+    upper::Int
+end
+
+
+struct Entry
+    policy::Policy
+    pw::String
+end
+
+
+function entry_is_valid(entry::Entry)
+    n = count(i -> i == entry.policy.letter, entry.pw)
+    entry.policy.lower <= n <= entry.policy.upper
+end
+
+
+main()
