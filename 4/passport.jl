47f76ecdb63aab01e2b39134fd2d7a85097175f7
rvforest <rvforest@gmail.com>
2020-12-20 08:26:31 -0500

    D4-p1: Count valid passports

--- 4/passport.jl
+++ 4/passport.jl
@@ -1,1 +1,14 @@
 Passport = Dict{SubString{String}, SubString{String}}
+
+REQUIRED_FIELDS = (
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid"
+)
+
+count_valid(pps::Array{Passport, 1}) = sum(is_valid.(pps))
+is_valid(pp::Passport) = all(in.(REQUIRED_FIELDS, Ref(keys(pp))))
--- 4/test.jl
+++ 4/test.jl
@@ -3,13 +3,21 @@ using Test
 include("input.jl")
 include("passport.jl")
 
+passports = parse_input("4/example.txt")
+
+
 function test_parse_input()
-    passports = parse_input("4/example.txt")
-    println(passports)
     @test length(passports) == 4
     pp = passports[1]
     @test isa(pp, Passport)
     @test pp["hcl"] == "#fffffd"
 end
 
+
+function test_validity_count()
+    @test count_valid(passports) == 2
+end
+
+
 test_parse_input()
+test_validity_count()
